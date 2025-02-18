4ec589eefe988b0012315896d13b62348b45485f
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-18 04:37:42 +0000

    trying to add in details for directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,14 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list"})
   end
+
+  def show
+    d_id = params.fetch("d_id")
+
+    matching_records = Director.where({ :id => "d_id"})
+
+    @the_director = matching_records.at(0)
+
+    render({ :template => "director_templates/details"})
+  end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index"})
 
+  get("/directors/:d_id", { :controller => "directors", :action => "show"})
+
 end
