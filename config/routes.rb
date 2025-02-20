01a997fb72ce059b4507fdbec3e7709769984f35
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-20 02:29:54 +0000

    adding actors and movies

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,14 @@ Rails.application.routes.draw do
 
   get("/directors/:d_id", { :controller => "directors", :action => "show"})
 
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/movies/:the_id", { :controller => "movies", :action => "show" })
+
+  get("/actors", { :controller => "actors", :action => "index" })
+
+  get("/actors/:the_id", { :controller => "actors", :action => "show" })
+
+  get("/directors/youngest", { :controller => "directors", :action => "junior" })
+
 end
